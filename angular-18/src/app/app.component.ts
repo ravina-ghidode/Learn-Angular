4d352762a6de6abf5abe56492b31c8ab51843f6e
ravina-ghidode <ravina26ghidode@gmail.com>
2025-01-23 12:46:49 +0530

    Attribute Directive : ngClass, ngStyle

--- angular-18/src/app/app.component.ts
+++ angular-18/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { StucturalDirectiveComponent } from './directives/stuctural-directive/stuctural-directive.component';
+import { AttributeDirectiveComponent } from './directives/attribute-directive/attribute-directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataBindingComponent, StucturalDirectiveComponent],
+  imports: [RouterOutlet, DataBindingComponent, StucturalDirectiveComponent, AttributeDirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-18/src/app/directives/attribute-directive/attribute-directive.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-attribute-directive',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './attribute-directive.component.html',
+  styleUrl: './attribute-directive.component.css'
+})
+export class AttributeDirectiveComponent {
+  div1BgColor: string = '';
+  isDiv2Active: boolean = false;
+  num1: string = "";
+  num2: string = "";
+  isActive: boolean = false;
+  studentList: any[] = [
+    { studentID: 1, totalMarks: 23, gender: "male", name: 'AAA', city: 'Pune', isActive: false },
+    { studentID: 2, totalMarks: 43, gender: "male", name: 'BBB', city: 'Pune', isActive: false },
+    { studentID: 3, totalMarks: 13, gender: "female", name: 'CCC', city: 'Pune', isActive: true },
+    { studentID: 4, totalMarks: 53, gender: "male", name: 'DDD', city: 'Pune', isActive: false },
+    { studentID: 5, totalMarks: 83, gender: "female", name: 'EEE', city: 'Pune', isActive: false }
+  ]
+  customerStyle: any = {
+    'color': 'white',
+    'background-color': 'red',
+    'width': '200px',
+    'height': '200px'
+  }
+  addRedClass() {
+    this.div1BgColor = 'bg-danger';
+  }
+  addblueClass() {
+    this.div1BgColor = 'bg-primary';
+  }
+  toggleDiv2() {
+    this.isDiv2Active = !this.isDiv2Active;
+  }
+}
